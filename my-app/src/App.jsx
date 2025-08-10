0ab80cac466e202c7f27555711dc978f04fbc726
Uddhav921 <uddhavtour6@gmail.com>
2025-08-10 13:28:07 +0530

    created myapp using vite & tried to import & export , print text

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+
+import Uddhav from './index'
+function App() {
+ 
+
+  return (
+    <>
+     <Uddhav/>
+     <h2>This is from App.js</h2>
+    </>
+  )
+}
+
+export default App
---
+++ my-app/src/index.jsx
@@ -0,0 +1,7 @@
+function Uddhav(){
+    return(
+        <h1>uddhav here from react</h1>
+    )
+}
+
+export default Uddhav
